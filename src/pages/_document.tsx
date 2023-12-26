dfa8c8354e458686c7de185615ba269b6521efa6
Jon Sundin <sundinjon@gmail.com>
2023-12-25 19:29:55 -0800

    added google maps api script and polyfill

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <Script src="https://polyfill.io/v3/polyfill.min.js?features=default"></Script>
+      </Head>
+      <body className="h-svh">
         <Main />
         <NextScript />
+        <Script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}&callback=initMap&v=weekly`} strategy="afterInteractive"></Script>
       </body>
     </Html>
   )
