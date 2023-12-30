09a53b25cb4f31c5f65fe4a04cdbacf43f24cbeb
Jon Sundin <sundinjon@gmail.com>
2023-12-29 16:43:48 -0800

    data in public dir

---
+++ public/toilets.sql
@@ -0,0 +1,16 @@
+CREATE TABLE toilets (
+	id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	site VARCHAR(255),
+	address VARCHAR(255),
+	latitude DECIMAL(9,6),  -- Latitude (9 total digits, 6 after the decimal)
+    longitude DECIMAL(9,6),  -- Longitude (9 total digits, 6 after the decimal)
+	hours VARCHAR(100),
+	cleaning_frequency VARCHAR(100),
+	public BOOLEAN,
+	content TEXT,
+	images VARCHAR(255)[],  -- storing images as an array of url strings
+	operated_by VARCHAR(100),
+	author_id INTEGER REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
